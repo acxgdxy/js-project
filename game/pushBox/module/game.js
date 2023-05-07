dc0158229737e09331ee19c17541911ebf6fb6e6
acxgdxy <1936983174@qq.com>
2023-05-07 14:31:32 +0800

    pushBox

---
+++ game/pushBox/module/game.js
@@ -0,0 +1,36 @@
+import { playerMove, isWin } from "./play.js";
+import showUI from "./ui.js";
+
+showUI();
+/**
+ * 测试用的
+ * window.playerMove = playMove
+ * window.showUI = showUI
+ */
+var over = false;
+//完成整个游戏
+
+window.onkeydown = function (e) {
+    if (over) {
+        return;
+    }
+    var result = false;
+    if (e.key === "ArrowUp") {
+        result = playerMove("up");
+    } else if (e.key === "ArrowDown") {
+        result = playerMove("down");
+    } else if (e.key === "ArrowLeft") {
+        result = playerMove("left");
+    } else if (e.key === "ArrowRight") {
+        result = playerMove("right");
+    }
+
+    if (result) {
+        showUI();
+        if (isWin()) {
+
+            over = true;
+            alert("游戏胜利！");
+        }
+    }
+};
